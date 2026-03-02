91e02cbb73385047296641a7a634110fcac61bda
punn-sirinut <write2punn@gmail.com>
2026-03-02 15:05:40 +0700

    02/03/2026

--- Python/A1-53.py
+++ Python/A1-53.py
@@ -1,10 +1,10 @@
-def mixColor(c1,c2):
-    return (c1+c2)//2
+def mixColor(c1,c2):return (c1+c2)//2
 def mixRGB(r1,g1,b1,r2,g2,b2):
     rMix=mixColor(r1,r2)
     gMix=mixColor(g1,g2)
     bMix=mixColor(b1,b2)
     return rMix,gMix,bMix
-r1,g1,b1,r2,g2,b2=map(int,input().split(" "))
+r1,g1,b1=map(int,input().split(" "))
+r2,g2,b2=map(int,input().split(" "))
 a,b,c=mixRGB(r1,g1,b1,r2,g2,b2)
 print(a,b,c)
