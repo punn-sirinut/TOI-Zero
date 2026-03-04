49f56bbb12e53b5f12847196866bf19173eb7ffb
punn-sirinut <write2punn@gmail.com>
2026-03-04 19:21:35 +0700

    04/03/2026

--- Python/A1-05.py
+++ Python/A1-05.py
@@ -1,6 +1,20 @@
-s=input()
-vovels=['a','e','i','o','u']
-if s in vovels:
-    print("yes")
+m=int(input())
+d=int(input())
+if d>=21:
+    if m==12 or m==1 or m==2:
+        print("winter")
+    elif m==3 or m==4 or m==5:
+        print("spring")
+    elif m==6 or m==7 or m==8:
+        print("summer")
+    elif m==9 or m==10 or m==11:
+        print("fall")
 else:
-    print("no")
+    if m==1 or m==2 or m==3:
+        print("winter")
+    elif m==4 or m==5 or m==6:
+        print("spring")
+    elif m==7 or m==8 or m==9:
+        print("summer")
+    elif m==10 or m==11 or m==12:
+        print("fall")
